94749751afa63a10454ce9e6f3a38c8029a20b8a
Ilya Dokuchaev <miamorokor@mail.ru>
2023-06-22 20:24:42 +0300

    enviroment configuration

---
+++ configuration/enviroment.js
@@ -0,0 +1,13 @@
+const path = require('path')
+
+module.exports = {
+    paths:{
+        source: path.resolve(__dirname,'../src/'),
+        output: path.resolve(__dirname,'../dist/')
+    },
+    server:{
+        host:'localhost',
+        port:'3000'
+    }
+
+}

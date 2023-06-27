e1c62ec8fccc66bef26ffeff0db90f0c34a9793a
Ilya Dokuchaev <miamorokor@mail.ru>
2023-06-27 15:35:10 +0300

    browserslist last two versions >5 %
    Signed-off-by: Ilya Dokuchaev <miamorokor@mail.ru>

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
                     loader: "babel-loader",
                     options: {
                         presets: [
-                            ['@babel/preset-env', {targets: "defaults"}]
+                            ['@babel/preset-env']
                         ]
                     }
                 }

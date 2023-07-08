ae013bc6eae5a580a3b54befe0e766312c31c65d
Ilya Dokuchaev <miamorokor@mail.ru>
2023-07-08 12:41:15 +0300

    minor bug fixes
    Signed-off-by: Ilya Dokuchaev <miamorokor@mail.ru>

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,28 @@
-import {Counter} from "./counter.js";
+/*
+ * MIT License
+ *
+ * Copyright (c) [year] [fullname]
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import Counter from "./counter.js";
 import '../scss/app.scss';
 
 const counter = new Counter(10);
@@ -10,4 +34,5 @@ counter.increment();
 
 // eslint-disable-next-line
 console.log(counter.getCounter());
-export {counter}
+
+export default counter;
